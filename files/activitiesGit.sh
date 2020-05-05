947d5e7f7a30a57e8220b6d20bc45823913c5a2f
rrojas <rrojas@paraguayeduca.org>
2020-05-04 22:23:25 -0400

    Git Clone 35 Sugar Activity

---
+++ files/activitiesGit.sh
@@ -0,0 +1,150 @@
+#! /bin/sh root
+
+sudo mkdir downAct
+cd downAct/
+
+#-   1 -
+# Turtle Artivity
+ git clone  https://github.com/sugarlabs/turtleart-activity.git
+
+#-   2 -
+# Clock Artivity
+ git clone  https://github.com/sugarlabs/clock-activity.git
+
+#-   3 -
+# Finance Artivity
+ git clone https://github.com/sugarlabs/finance-activity.git
+
+#-   4 -
+# Write Artivity
+ git clone https://github.com/sugarlabs/write-activity.git
+
+#-   5 -
+# Read Artivity
+ git clone https://github.com/sugarlabs/read-activity.git
+
+#-   6 -
+# Abaco Artivity
+git clone https://github.com/sugarlabs/activity-abacus.git
+
+#-   7 -
+# ImageView Artivity
+git clone https://github.com/sugarlabs/imageviewer-activity.git
+
+#-   8 -
+# Paint Artivity
+git clone https://github.com/sugarlabs/paint-activity.git
+
+#-   9 -
+# Clock Artivity
+
+
+#-   10 -
+# Memorize Artivity
+git clone https://github.com/sugarlabs/memorize-activity.git
+
+#-   11 -
+# Words Artivity
+git clone https://github.com/sugarlabs/words-activity.git 
+
+#-   12 -
+# Maze Artivity
+git clone  https://github.com/sugarlabs/maze-activity.git
+
+#-   13 -
+# Calculate Artivity
+git clone https://github.com/sugarlabs/calculate-activity.git
+
+#-   14 -
+# Chat Artivity
+git clone https://github.com/sugarlabs/chat.git
+
+#-   15 -
+# Moon Artivity
+git clone https://github.com/sugarlabs/moon-activity.git
+
+#-   16 -
+# Story Artivity
+git clone https://github.com/sugarlabs/story.git
+
+#-   17 -
+# Measure Artivity
+git clone https://github.com/sugarlabs/Measure.git
+
+#-   18 -
+# labyrinth-activity (mapMent)
+git clone https://github.com/sugarlabs/labyrinth-activity.git
+
+#-   19 -
+# I can read Artivity
+git clone https://github.com/sugarlabs/i-can-read-activity.git
+
+#-   20 -
+# xoeditor Artivity
+git clone https://github.com/sugarlabs/xoeditor-activity.git
+
+#-   21 -
+# Get Books Artivity
+git clone  https://github.com/sugarlabs/get-books-activity.git
+
+#-   22 -
+# Gears Artivity#
+git clone https://github.com/sugarlabs/gears-activity.git
+
+#-   23 -
+# Develop Artivity
+git clone https://github.com/sugarlabs/develop-activity.git
+
+#-   24 -
+# Help Artivity
+git clone https://github.com/sugarlabs/help-activity.git
+
+#-   25 -
+# Fractionauts Artivity
+git clone https://github.com/sugarlabs/fractionauts.activity.git
+
+#-   26 -
+# Derecho a Transitar Artivity
+git clone https://github.com/sugarlabs/derecho-a-transitar-activity.git
+
+#-   27 -
+# Jam Tank Artivity
+git clone https://github.com/sugarlabs/jam-tank-activity.git
+
+#-   28 -
+# Jam Media explorer Artivity
+git clone https://github.com/sugarlabs/jam-media-explorer-activity.git
+
+#-   29 -
+# Vnc Launcher Artivity
+git clone https://github.com/sugarlabs/VncLauncher.git
+
+#-   30 -
+# Jclic Artivity
+git clone https://github.com/sugarlabs/JClic.git
+
+
+
+#-   31 -
+# Turtle confusion Artivity
+git clone https://github.com/sugarlabs/activity-turtle-confusion.git
+
+
+
+#-   32 -
+# Panorama Artivity
+git clone  https://github.com/sugarlabs/panorama.git
+
+
+
+#-   33 -
+# Tuxpaint Artivity
+git clone  https://github.com/sugarlabs/tuxpaint-wrapper-activity.git
+
+#-   34 -
+# Turtle art Artivity
+git clone  https://github.com/sugarlabs/activity-turtleart-extras.git
+
+#-   35 -
+# Diccionario Guarani - Espanol Artivity
+git clone  https://github.com/sugarlabs/diccionario-guarani-castellano-activity.git
