2a51aa814880e53d71aa711615da0475f1814c3d
Angatupyry <fierrofenix@gmail.com>
2020-06-04 10:04:27 -0400

    activitiesGit fix

--- config/activitiesGit.sh
+++ config/activitiesGit.sh
@@ -1,28 +1,20 @@
 # ! /bin/bash root
 
+#The function loops through the json file and clones the activity repository based on it's version or latest commit
 for k in $(jq '.repository | keys | .[]' repository.json); do
     value=$(jq -r ".repository[$k]" repository.json)
     repositoryName=$(jq -r '.name' <<<"$value")
     link=$(jq -r '.link' <<<"$value")
     flagVersion=$(jq -r 'if .version == "" then 0 else 1 end' <<<"$value")
-    echo $flagVersion
-    echo $repositoryName
-    echo $link
+
     if [ $flagVersion == 0 ]; then
         commit=$(jq -r '.last_commit' <<<"$value")
-        echo $commit
         git clone $link
         cd $repositoryName
         git checkout $commit
         cd ..
     else
         version=$(jq -r '.version' <<<"$value")
-        echo $link
         git clone -b $version $link
     fi
 done
-
-#- 10 -
-# KukuAnakula Activity
-#git clone git://git.sugarlabs.org/kukuanakula/mainline.git
-
