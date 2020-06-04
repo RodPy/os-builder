c45bd7cc7cefbff21dca7820c402968d4b451efb
Angatupyry <fierrofenix@gmail.com>
2020-06-03 23:04:08 -0400

    filter objects with jq

--- config/activitiesGit.sh
+++ config/activitiesGit.sh
@@ -1,4 +1,26 @@
-#!/bin/sh root
+# ! /bin/bash root
+
+for k in $(jq '.repository | keys | .[]' repository.json); do
+    value=$(jq -r ".repository[$k]" repository.json)
+    repositoryName=$(jq -r '.name' <<<"$value")
+    link=$(jq -r '.link' <<<"$value")
+    flagVersion=$(jq -r 'if .version == "" then 0 else 1 end' <<<"$value")
+    echo $flagVersion
+    echo $repositoryName
+    echo $link
+    if [ $flagVersion == 0 ]; then
+        commit=$(jq -r '.last_commit' <<<"$value")
+        echo $commit
+        git clone $link
+        cd $repositoryName
+        git checkout $commit
+        cd ..
+    else
+        version=$(jq -r '.version' <<<"$value")
+        echo $link
+        git clone -b $version $link
+    fi
+done
 
 #GIT
 #- 1 -
@@ -59,21 +81,17 @@ git clone -b v19 https://github.com/sugarlabs/get-books-activity.git
 # Turtle confusion Activity
 git clone https://github.com/sugarlabs/activity-turtle-confusion.git
 
-
 #-   1 -
 # Turtle Art Artivity
- git clone -b v220 https://github.com/sugarlabs/turtleart-activity.git
-
+git clone -b v220 https://github.com/sugarlabs/turtleart-activity.git
 
 #- -
 #Jclic  Activity
 git clone -b v1 https://github.com/sugarlabs/JClic.git
 
-
 #-  -
 #Java  Activity
 git clone https://github.com/sugarlabs/Java.git
-
 
 #Etoys  Activity
 git clone https://github.com/ParaguayEduca/etoys.activity.git
